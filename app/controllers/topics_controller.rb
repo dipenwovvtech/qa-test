e3294cca7bd1266b40a29ecdfb6b20459df18e3c
Dipen Bhanushali <dipen.bhanushali@gmail.com>
2020-04-19 15:55:52 +0530

    added friendly id to topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -64,7 +64,7 @@ class TopicsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
-      @topic = Topic.find(params[:id])
+      @topic = Topic.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,12 @@
 class Topic < ApplicationRecord
+
+  validates :name, presence: true
+
+  extend FriendlyId
+  friendly_id :secure_slug, use: :slugged
+
+
+  def secure_slug
+    @random_slug ||=persisted? ? friendly_id: "#{Time.now.to_i}-#{name.parameterize}"
+  end
 end
